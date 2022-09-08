a75ff23733031d31c9952c99d1d40f6c53c7a120
Usama Khan <khanusama20@gmail.com>
2022-09-09 01:28:55 +0530

    Postgresql connection established successfully

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+// db connection
+const pool = require('./config/connection.postgresql');
+
+try {
+  pool.query('SELECT $1::text as message', ['Database connection established!'], (err, res) => {
+    console.log(err ? err.stack : res.rows[0].message); // Hello World!
+  })
+} catch (error) {
+  console.log("Something went wrong while connecting to databases");
+}
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
