5a52829979e0e66fb2e20502d66e60af6badb3a5
Usama Khan <khanusama20@gmail.com>
2022-09-20 22:07:28 +0530

    login API

--- src/api/users/user.controller.js
+++ src/api/users/user.controller.js
@@ -1,7 +1,9 @@
 const moment = require('moment');
+const bcrypt = require('bcrypt');
 const { UserJoiSchema } = require('./user.joi');
 const resManager = require('../../utilities/responseManager'); // resManager = Response Managaer
 const pool = require('../../config/connection.postgresql');
+const pattern = require('../../constants/pattern.regex');
 
 /**
  * 
@@ -16,6 +18,9 @@ const agentSignUp = async (req, res) => {
     let date = moment().format();
 
     age = age.split(' ')[0];
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
 
     const insertQuery = `
     INSERT INTO agents(
@@ -34,8 +39,9 @@ const agentSignUp = async (req, res) => {
       created_at,
       updated_at,
       login_count,
-      last_login_date
-    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16)
+      last_login_date,
+      salt
+    ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
     RETURNING *;
     `;
 
@@ -48,24 +54,23 @@ const agentSignUp = async (req, res) => {
       data.gender,
       data.birth_date,
       age,
-      data.password,
+      hash,
       data.emp_code,
       data.state,
       data.city,
       date,
       date,
       0,
-      null
+      null,
+      salt
     ];
 
     pool.query(insertQuery, values, (error, result) => {
       if (error) {
         console.error(error.stack)
         resManager.DatabaseError(req, res, 'Database error: ' + error.message);
-        pool.end();
       } else {
         resManager.success(req, res, result.rows);
-        pool.end();
       }
     });
   } catch (error) {
@@ -78,6 +83,42 @@ const agentSignUp = async (req, res) => {
   }
 }
 
+const login = async (req, res) => {
+  try {
+    const data = req.body;
+    const query = 'SELECT salt, password, login_count FROM agents WHERE pan_no = $1';
+    pool.query(query, [data.username], async (error, result) => {
+      if (error) {
+        console.error(error.stack)
+        resManager.DatabaseError(req, res, 'Database error: ' + error.message);
+      } else if (result.rows.length > 0) {
+        let salt = result.rows[0].salt;
+        const hash = bcrypt.hashSync(data.password, salt);
+        if (hash !== result.rows[0].password) {
+          resManager.Unauthorized(req, res, 'Invalid Credentials')
+        } else {
+          try {
+            let login_count = result.rows[0].login_count + 1;
+            let date = moment().format();
+            
+            let user_details = await pool
+              .query('UPDATE agents SET last_login_date = $1, login_count = $2 WHERE pan_no = $3 RETURNING *;',
+                [date, login_count, data.username]
+              );
+            resManager.success(req, res, user_details.rows);
+          } catch (error) {
+            resManager.InternalServerError(req, res, error.message);
+          }
+        }
+      } else {
+        resManager.success(req, res, result.rows);
+      }
+    })
+  } catch (error) {
+    resManager.InternalServerError(req, res, error.message);
+  }
+}
 module.exports = {
-  agentSignUp
+  agentSignUp,
+  login
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,7 @@ let authRoutes = express.Router();
 const user = require('../api/users/user.controller');
 
 authRoutes.post('/register-agent', user.agentSignUp);
+authRoutes.post('/login', user.login);
 
 module.exports = [
   authRoutes,
